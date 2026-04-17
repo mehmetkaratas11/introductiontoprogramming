c33bc4c3202fdf2f5f50dc9a6bf522c1c9baf4b1
mehmetkaratas11 <mehmetkaratas@ibu.edu.mk>
2026-04-17 22:52:53 +0000

    Sat, Apr 18, 2026, 12:52 AM +02:00

--- week4/filter-less/helpers.h
+++ week4/filter-less/helpers.h
@@ -1,16 +1,10 @@
-// CS50x Week 4 — Filter (Less): helpers.h
-// DO NOT MODIFY THIS FILE
+#ifndef HELPERS_H
+#define HELPERS_H
 
 #include "bmp.h"
 
-// Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
-
-// Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width]);
-
-// Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width]);
-
-// Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width]);
+
+#endif
